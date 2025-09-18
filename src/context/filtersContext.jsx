fc30cf699acfcbce8f86c320f61a3b0f563d4d36
Sergio Sacristan <sergiosacristan8@gmail.com>
2025-09-18 16:29:16 +0200

    fixed

--- src/context/filtersContext.jsx
+++ src/context/filtersContext.jsx
@@ -11,4 +11,5 @@ export const FiltersProvider = ({ children }) => {
     );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useFilters = () => useContext(filtersContext);
